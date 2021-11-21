e707dfdd95821ed1585187ac4d9573dc1f2b4381
Aidan <aidanc@rogers.com>
2021-11-21 16:12:37 -0500

    Add header item title

--- src/index.js
+++ src/index.js
@@ -11,10 +11,16 @@ function HeaderItem(props) {
     );
 }
 
+function HeaderItemTitle(props) {
+    return (
+        <h1><a className="header-item-title" href={props.link}>{props.text}</a></h1>
+    );
+}
+
 function Header(props) {
     return (
         <header className="header">
-            <h1>Aidan Clyens</h1>
+            <HeaderItemTitle text="Aidan Clyens" link="#" />
             <nav>
                 <HeaderItem text="About" link="#about" />
                 <HeaderItem text="Projects" link="#" />
