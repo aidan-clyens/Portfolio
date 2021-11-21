28a3f194bff20bd0ce5d148eb86daae34a7ec43b
Aidan <aidanc@rogers.com>
2021-11-21 13:10:15 -0500

    Add Home section to body

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import background_image from './background_image.jpg'
+import profile_image from './profile.jpeg'
 
 
 function HeaderItem(props) {
@@ -25,10 +27,23 @@ class Header extends React.Component {
 }
 
 
+function HomeSection(props) {
+    return (
+        <div className="home">
+            <img className="home-background" src={background_image} />
+            <img className="home-profile" src={profile_image} />
+            <div className="home-intro-text">
+                Hi! My name is Aidan Clyens. I am a junior software engineer and recent graduate from the University of Waterloo.
+            </div>
+        </div>
+    );
+}
+
 class Body extends React.Component {
     render() {
         return (
             <div className="body">
+                <HomeSection />
             </div>
         );
     }
